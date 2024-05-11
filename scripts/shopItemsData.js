23fdf02c4e7d25636d15e1c1358e0f57d8c77572
lukaNikolava <nikolavaluka475@gmail.com>
2024-05-12 00:15:09 +0400

    primitiuli database

---
+++ scripts/shopItemsData.js
@@ -0,0 +1,70 @@
+const Items = [
+    {
+        thing: 'Hat',
+        new: 'false',
+        price:'5$',
+        color:'black'
+    },
+    {
+        thing: 'shoes',
+        new: 'true',
+        price:'12$',
+        color:'black'
+    },
+    {
+        thing: 'shirt',
+        new: 'false',
+        price:'25$',
+        color:'white'
+    },
+    {
+        thing: 'shirt',
+        new: 'false',
+        price:'15$',
+        color:'yellow'
+    },
+    {
+        thing: 'skirt',
+        new: 'true',
+        price:'55$',
+        color:'yellow'
+    },
+    {
+        thing: 'hoodie',
+        new: 'true',
+        price:'25$',
+        color:'black'
+    },
+    {
+        thing: 'skirt',
+        new: 'false',
+        price:'15$',
+        color:'black'
+    },
+    {
+        thing: 'Hat',
+        new: 'true',
+        price:'27$',
+        color:'black'
+    },
+    {
+        thing: 'hoodie',
+        new: 'false',
+        price:'20$',
+        color:'black'
+    },
+    {
+        thing: 'skirt',
+        new: 'true',
+        price:'25$',
+        color:'black'
+    },
+    {
+        thing: 'shirt',
+        new: 'false',
+        price:'35$',
+        color:'black'
+    }
+];
+
+export default Items;
